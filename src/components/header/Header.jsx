5b390f649906d2ca08650a096825c4483a77e437
omarYousifKamal <omaryousf688@gmail.com>
2023-03-24 05:52:13 +0300

    added header, mob-nav(50% completed)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,39 +9,39 @@ const Header = () =>
                 <div className='nav__menu'>
                     <ul className="nav__list grid">
                     <li className="nav__item">
-                        <a href="#home" className="nav__link">
-                            <i className='uil uil-estate nav-icon'></i>
+                        <a href="#home" className="nav__link" active-link>
+                            <i className='uil uil-estate nav__icon'></i>
                             Home
                         </a>
                     </li>
                     <li className="nav__item">
                         <a href="#about" className="nav__link">
-                            <i className='uil uil-user nav-icon'></i>
+                            <i className='uil uil-user nav__icon'></i>
                             About
                         </a>
                     </li>
                     <li className="nav__item">
                         <a href="#skills" className="nav__link">
-                            <i className='uil uil-file-alt nav-icon'></i>
+                            <i className='uil uil-file-alt nav__icon'></i>
                             Skills
                         </a>
                     </li>
                     <li className="nav__item">
                         <a href="#services" className="nav__link">
-                            <i className='uil uil-briefcase-alt nav-icon'></i>
+                            <i className='uil uil-briefcase-alt nav__icon'></i>
                             Services
                             
                         </a>
                     </li>
                     <li className="nav__item">
                         <a href="#portfolio" className="nav__link">
-                            <i className='uil uil-scenery nav-icon'></i>
+                            <i className='uil uil-scenery nav__icon'></i>
                             Portfolio
                         </a>
                     </li>
                     <li className="nav__item">
                         <a href="#contact" className="nav__link">
-                            <i className='uil uil-message nav-icon'></i>
+                            <i className='uil uil-message nav__icon'></i>
                             Contact
                         </a>
                     </li>
