b0816469bdeffaa9f2da18d3676873fdcac9bd3b
omarYousifKamal <omaryousf688@gmail.com>
2023-03-25 09:17:32 +0300

    start from social

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Home = () => {
+    return(
+        <section className="home section" id="home">
+            <div className="home__container container grid">
+                <div className="home__content grid">
+                    
+                </div>
+            </div>
+        </section>
+    );
+}
+export default Home
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="" className="home__social-icon"></a>
+    </div>
+  )
+}
+
+export default Social
